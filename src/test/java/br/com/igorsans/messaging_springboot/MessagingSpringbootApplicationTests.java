5917e64d8ee44a90dcd4f5febe12aac92c1dc5c9
Igor Santos <igor.santos0@hotmail.com>
2024-11-12 09:11:31 -0300

    done

---
+++ src/test/java/br/com/igorsans/messaging_springboot/MessagingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igorsans.messaging_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
